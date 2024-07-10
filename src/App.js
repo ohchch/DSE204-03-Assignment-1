4b276a46e86dce1583937c14339c694620195987
ohchch <ohchiachun001@gmail.com>
2024-07-10 10:54:50 +0800

    002

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Car from './car'; 
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       hello world!
+      <Car />
       </header>
     </div>
   );
---
+++ src/car.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Car() {
+  return <h3>I am a car</h3>;
+}
+
+export default Car;
